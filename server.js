9dc4b5c76d622798881226bc2751b2dcbc7e5c8f
beteh11 <beteh_11@hotmail.com>
2024-08-15 21:29:36 +0700

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const net = require('net');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 10000;
 
 // Use CORS middleware
 app.use(cors());
